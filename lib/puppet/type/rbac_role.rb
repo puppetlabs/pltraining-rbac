93243f3a9e14285d5f55010867c4fb9c8719ba19
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-09-09 19:52:33 +1000

    Added better logic for comparing arrays of hashes

--- lib/puppet/type/rbac_role.rb
+++ lib/puppet/type/rbac_role.rb
@@ -17,7 +17,14 @@ Puppet::Type.newtype(:rbac_role) do
       # The current value may be nil and we don't
       # want to call sort on it so make sure we have arrays
       if is.is_a?(Array) and @should.is_a?(Array)
-        is.sort == @should.sort
+        # if all the elements are hashes then we can compare them using this
+        # nice method. If  not we are going to have to rely on ==
+        if (is + @should).all? { |x| x.is_a?(Hash) }
+          diff = (is - @should) + (@should - is)
+          diff.empty?
+        else
+          is.sort == @should.sort
+        end
       else
         is == @should
       end
